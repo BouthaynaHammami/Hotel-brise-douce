4f35cf63d4dd722e015647a226265469544041dd
Bouthayna Hammami <Bouthayna.Hammami@esprit.tn>
2026-03-21 23:51:43 +0100

    Back_End

---
+++ DevUnity_BriseDouce/Gestion_personnel/src/main/java/tn/esprit/personnel_service/PersonnelApplication.java
@@ -0,0 +1,11 @@
+package tn.esprit.personnel_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonnelApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PersonnelApplication.class, args);
+    }
+}
---
+++ DevUnity_BriseDouce/Gestion_reservations/src/main/java/tn/esprit/reservations_service/ReservationsApplication.java
@@ -0,0 +1,11 @@
+package tn.esprit.reservations_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationsApplication.class, args);
+    }
+}
